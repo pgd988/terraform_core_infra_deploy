9368e682ee1845df674afd7b6e12108c4b52e951
Denis Beloivanov <pgd988@gmail.com>
2026-04-24 11:47:54 +0200

    update

--- gke.tf
+++ gke.tf
@@ -43,7 +43,12 @@ resource "google_container_node_pool" "default_pool" {
     }
 
     oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform"
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/trace.append",
     ]
     tags = ["gke-node"]
   }
@@ -70,7 +75,12 @@ resource "google_container_node_pool" "apps_pool" {
     }
 
     oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform"
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/trace.append",
     ]
     tags = ["gke-node", "app"]
   }
